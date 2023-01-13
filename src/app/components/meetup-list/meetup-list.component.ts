6a1aa15642915dd9ab8af4801c14b8cb8e6ad887
lemov nikita <lemov1980@yandex.ru>
2023-01-13 13:11:12 +0300

    reloaded meetups is done

--- src/app/components/meetup-list/meetup-list.component.ts
+++ src/app/components/meetup-list/meetup-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscribable, Subscription } from 'rxjs';
 import { IMeetup } from 'src/app/model/meetup';
 import { IUser } from 'src/app/model/user';
 import { AuthService } from 'src/app/services/auth.service';
@@ -13,6 +14,7 @@ import { MeetupService } from 'src/app/services/meetup.service';
 export class MeetupListComponent implements OnInit {
   meetups: IMeetup[] = [];
   userInfo!: IUser | null;
+  subscription!: Subscription;
   constructor(
     private meetupService: MeetupService,
     private authService: AuthService,
@@ -26,19 +28,33 @@ export class MeetupListComponent implements OnInit {
   getMeetups() {
     this.userInfo = this.authService.user;
     if (this.router.url.includes('my-meetups')) {
-      this.meetupService.getAll().subscribe((meetups) => {
+      this.subscription = this.meetupService.getAll().subscribe((meetups) => {
         this.meetups = meetups.filter(
           (meetup) => meetup.createdBy === this.userInfo?.id
         );
       });
     } else {
-      this.meetupService.getAll().subscribe((meetups) => {
+      this.subscription = this.meetupService.getAll().subscribe((meetups) => {
         this.meetups = meetups;
       });
     }
   }
 
+  refreshMeetups() {
+    this.subscription = this.meetupService.reloadMeetups.subscribe((value) => {
+      this.meetups = value;
+      console.log(value, 'from reloader');
+    });
+    this.meetupService.refreshMeetups();
+  }
+
   ngOnInit(): void {
     this.getMeetups();
+    this.refreshMeetups();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+    this.meetupService.timerDestroy();
   }
 }
--- src/app/services/meetup.service.ts
+++ src/app/services/meetup.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { filter, Observable } from 'rxjs';
+import { filter, Observable, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { IMeetup } from '../model/meetup';
 
@@ -8,6 +8,8 @@ import { IMeetup } from '../model/meetup';
   providedIn: 'root',
 })
 export class MeetupService {
+  reloadMeetups = new Subject<IMeetup[]>();
+  timer: any;
   private baseUrl: string = `${environment.backendOrigin}/meetup`;
   constructor(private http: HttpClient) {}
 
@@ -15,7 +17,19 @@ export class MeetupService {
     return this.http.get<any>(this.baseUrl);
   }
 
-  createMeetup(data: any) {
+  createMeetup(data: IMeetup) {
     return this.http.post(this.baseUrl, data);
+  }
+
+  refreshMeetups() {
+    this.timer = setInterval(() => {
+      return this.getAll().subscribe((meetup) => console.log(meetup));
+    }, 5000);
+  }
+
+  timerDestroy() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
   }
 }
